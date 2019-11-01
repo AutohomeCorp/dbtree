ea2c49af395545be546484be602d2c8e988a244a
kechangqing <kechangqing@autohome.com.cn>
2019-11-01 10:17:22 +0800

    字段注释为空时不加注释
    字段注释为空时不加注释

--- dbtree-backend/src/main/java/com/autohome/dbtree/util/CommentGeneratorExt.java
+++ dbtree-backend/src/main/java/com/autohome/dbtree/util/CommentGeneratorExt.java
@@ -1,19 +1,31 @@
 package com.autohome.dbtree.util;
 
+import com.google.common.base.Strings;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommentGeneratorExt extends DefaultCommentGenerator {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(CommentGeneratorExt.class);
 
     @Override
     public void addFieldComment(Field field,
                                 IntrospectedTable introspectedTable,
                                 IntrospectedColumn introspectedColumn) {
         String remarks = introspectedColumn.getRemarks();
-        field.addJavaDocLine("/**");
-        field.addJavaDocLine(" * " + remarks);
-        field.addJavaDocLine(" */");
+        if (Strings.isNullOrEmpty(remarks)) {
+            return;
+        }
+        try {
+            field.addJavaDocLine("/**");
+            field.addJavaDocLine(" * " + remarks);
+            field.addJavaDocLine(" */");
+        } catch (Exception ex) {
+            LOGGER.error("error when addFieldComment", ex);
+        }
     }
 }
