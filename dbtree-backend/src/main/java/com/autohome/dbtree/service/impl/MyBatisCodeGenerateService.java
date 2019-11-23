ab3e37cdde383ef6e6017666d32576353661cbdf
kechangqing <kechangqing@autohome.com.cn>
2019-11-23 15:58:59 +0800

    mybatis代码生成增加自增主键判断
    mybatis代码生成增加自增主键判断

--- dbtree-backend/src/main/java/com/autohome/dbtree/service/impl/MyBatisCodeGenerateService.java
+++ dbtree-backend/src/main/java/com/autohome/dbtree/service/impl/MyBatisCodeGenerateService.java
@@ -5,17 +5,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import javax.annotation.Resource;
 
 import com.autohome.dbtree.config.MybatisCodeGeneratorConfig;
+import com.autohome.dbtree.contract.ColumnInfo;
 import com.autohome.dbtree.contract.DbInfo;
 import com.autohome.dbtree.contract.DbServer;
 import com.autohome.dbtree.service.IMybatisCodeGenerateService;
+import com.autohome.dbtree.service.ITreeService;
 import org.joda.time.DateTime;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.CommentGeneratorConfiguration;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.Context;
+import org.mybatis.generator.config.GeneratedKey;
 import org.mybatis.generator.config.JDBCConnectionConfiguration;
 import org.mybatis.generator.config.JavaClientGeneratorConfiguration;
 import org.mybatis.generator.config.JavaModelGeneratorConfiguration;
@@ -38,6 +42,9 @@ public class MyBatisCodeGenerateService implements IMybatisCodeGenerateService {
 
     @Resource
     private Map<String, DbServer> dbServerMap;
+
+    @Resource
+    private ITreeService treeService;
 
     @Resource
     private MybatisCodeGeneratorConfig mybatisCodeGeneratorConfig;
@@ -76,7 +83,7 @@ public class MyBatisCodeGenerateService implements IMybatisCodeGenerateService {
         DbInfo dbInfo = dbInfoMap.get(dbName);
         DbServer dbServer = dbServerMap.get(dbInfo.getDb_server());
         JDBCConnectionConfiguration jdbcConnectionConfiguration = new JDBCConnectionConfiguration();
-        if(dbServer.getDb_type().equalsIgnoreCase("mysql")) {
+        if (dbServer.getDb_type().equalsIgnoreCase("mysql")) {
             jdbcConnectionConfiguration.setDriverClass("com.mysql.cj.jdbc.Driver");
             jdbcConnectionConfiguration.setConnectionURL(String.format("jdbc:mysql://%s:%s/%s", dbServer.getHost(), dbServer.getPort(), dbName));
         } else if (dbServer.getDb_type().equalsIgnoreCase("sqlserver")) {
@@ -130,8 +137,14 @@ public class MyBatisCodeGenerateService implements IMybatisCodeGenerateService {
             tableConfiguration.setUpdateByPrimaryKeyStatementEnabled(true);
             tableConfiguration.setSelectByPrimaryKeyStatementEnabled(true);
             tableConfiguration.setDeleteByPrimaryKeyStatementEnabled(true);
-            /*GeneratedKey generatedKey = new GeneratedKey("id", "mysql", true, null);
-            tableConfiguration.setGeneratedKey(generatedKey);*/
+
+            List<ColumnInfo> columnInfos = treeService.columnList(dbName, tableName);
+            Optional<ColumnInfo> primaryColumn = columnInfos.stream().filter(columnInfo -> columnInfo.getAutoIncrement() == 1 && columnInfo.getPrimaryKey() == 1)
+                    .findFirst();
+            if (primaryColumn.isPresent()) {
+                GeneratedKey generatedKey = new GeneratedKey(primaryColumn.get().getColumnName(), dbServer.getDb_type(), true, null);
+                tableConfiguration.setGeneratedKey(generatedKey);
+            }
 
             context.addTableConfiguration(tableConfiguration);
         }
